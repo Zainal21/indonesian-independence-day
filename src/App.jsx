d8ea8b90fa5ac427f15a29260a47aa6c7b8ac6a1
Zainal21 <Zainalarifin080718@gmail.com>
2022-08-16 15:15:44 +0700

    feat : clean up side effect execute

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 function App() {
-  const [isRendered, setIsRendered] = useState(true);
   useEffect(() => {
     return () => {
-      if (isRendered) {
-        let container = document.querySelector(".container-flag");
-        let containerSize = parseInt(getComputedStyle(container).width, 10);
-        for (let i = 0; i < containerSize; i++) {
-          let _flagElement = document.createElement("div");
-          _flagElement.classList.add("flag");
-          _flagElement.style.animationDelay = i * 10 + "ms";
-          container.appendChild(_flagElement);
-        }
-        setIsRendered(false);
+      let container = document.querySelector(".container-flag");
+      let containerSize = parseInt(getComputedStyle(container).width, 10);
+      for (let i = 0; i < containerSize; i++) {
+        let _flagElement = document.createElement("div");
+        _flagElement.classList.add("flag");
+        _flagElement.style.animationDelay = i * 10 + "ms";
+        container.appendChild(_flagElement);
       }
     };
   }, []);
